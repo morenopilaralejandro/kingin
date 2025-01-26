dda4eb8fc5d00a9da280b86db23f75dc8fa853b2
alejandro <morenopilaralejandro@gmail.com>
2025-01-26 21:54:19 +0100

    pd-n385

--- src/main/java/com/pocket/kingin/scraping/MainScraping.java
+++ src/main/java/com/pocket/kingin/scraping/MainScraping.java
@@ -16,11 +16,15 @@ import org.springframework.util.ResourceUtils;
 public class MainScraping {
 
 	public static void main(String[] args) {
-		scrapPdCombo();
+		scrapPdJa("n386", "Deoxys - Normal Forme");
+		scrapPdJa("n386a", "Deoxys - Attack Forme");
+		scrapPdJa("n386d", "Deoxys - Defense Forme");
+		scrapPdJa("n386s", "Deoxys - Speed Forme");
+		//scrapPdCombo();
 	}
 	
 	private static void scrapPdCombo() { 
-		int[] nums = {300, 301};
+		int[] nums = {385};
 		String[] names = new String[nums.length];
 		for (int i = 0; i < nums.length; i++) {
 			names[i] = scrapMoveEn("n"+nums[i], ""+nums[i]);
